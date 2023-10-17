a9ff16ddf819abef18719046d7825f51f1d19d65
Soumava Das <soumavadas06@gmail.com>
2023-10-17 17:10:48 +0000

    codewars

---
+++ sentence_smash.java
@@ -0,0 +1,25 @@
+/*
+Sentence Smash
+
+Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+Example
+
+['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+*/
+
+import java.util.Arrays;
+
+public class SmashWords {
+
+	public static String smash(String... words) {
+    String sentence = "";
+    for (String word : words) {
+      if (sentence.isEmpty()) {
+        sentence += word;
+      } else {
+        sentence += " " + word;
+      }
+      }
+        return sentence;
+    }
+  }
